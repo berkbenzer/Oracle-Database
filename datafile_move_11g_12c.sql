8221c52997b567ea8b387f06be810dec3ef82ad2
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-08-23 14:13:01 +0300

    Update datafile_move_11g_12c.sql

--- datafile_move_11g_12c.sql
+++ datafile_move_11g_12c.sql
@@ -4,6 +4,9 @@
 
 ALTER DATABASE DATAFILE '/u01/app/oracle/oradata/murat/users02.dbf' OFFLINE;
 
+
+mv '/u01/app/oracle/oradata/murat/users02.dbf' '/u01/arch/users2.dbf'
+
 ALTER DATABASE RENAME FILE '/u01/app/oracle/oradata/murat/users02.dbf' TO '/u01/arch/users2.dbf';
 
 ALTER DATABASE RECOVER DATAFILE '/u01/arch/users2.dbf';
