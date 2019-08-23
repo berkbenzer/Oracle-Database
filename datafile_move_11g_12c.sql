203b158f9a2b76cc337b9329f0767a37e52f0335
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-08-23 14:04:47 +0300

    Create datafile_move_11g_12c.sql

---
+++ datafile_move_11g_12c.sql
@@ -0,0 +1,16 @@
+--11g
+
+
+
+ALTER DATABASE DATAFILE '/u01/app/oracle/oradata/murat/users02.dbf' OFFLINE;
+
+ALTER DATABASE RENAME FILE '/u01/app/oracle/oradata/murat/users02.dbf' TO '/u01/arch/users2.dbf';
+
+ALTER DATABASE RECOVER DATAFILE '/u01/arch/users2.dbf';
+
+ALTER DATABASE DATAFILE '/u01/arch/users2.dbf' online;
+
+
+--12c 
+
+ALTER DATABASE MOVE DATAFILE '/u01/app/oracle/oradata/cdb1/system01.dbf' TO '/tmp/system01.dbf';
