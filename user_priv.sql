737298bafdbc7f3f3d81fcc84610c1ed570f8bb8
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-08-26 11:21:48 +0300

    Create user_priv.sql

---
+++ user_priv.sql
@@ -0,0 +1,12 @@
+SELECT
+  DSP.GRANTEE,DSP.PRIVILEGE,DU.ACCOUNT_STATUS
+FROM
+  DBA_SYS_PRIVS dsp,  SYS.DBA_USERS du
+  where 1=1
+  and DSP.GRANTEE = DU.USERNAME
+  and du.account_status= 'OPEN'
+  and du.username not in ('SYS','SYSTEM','DBSAT','APPDBCONN')
+ -- AND du.username ='BBENZER'
+  group by GRANTEE,PRIVILEGE,DU.ACCOUNT_STATUS
+  ORDER BY 1
+;
