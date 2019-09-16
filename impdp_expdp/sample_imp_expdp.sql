bfe2338e0c29b463a5291d363b1ba38b56817e32
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-09-16 10:14:20 +0300

    Update sample_imp_expdp.sql

--- impdp_expdp/sample_imp_expdp.sql
+++ impdp_expdp/sample_imp_expdp.sql
@@ -10,3 +10,6 @@ exp \'/ as sysdba\' file=/oracle/yedek/audit_27032019.dmp log=/oracle/yedek/audi
 exp '/ as sysdba' file=xxx_07122017.dmp log=xxx_07122017.log OWNER=TEST GRANTS=y ROWS=y
 
 imp '/ as sysdba' file=full64_07.12.2017.dmp log=full64_07.12.2017_imp.log fromuser=TEST touser=TEST ignore=y 
+
+
+expdp directory=pump dumpfile=XX0205.dmp logfile=XX0205.log schemas=XXX flashback_scn=11274316701577 estimate=statistics exclude=table:"in\('T_X1'\,'X2'\,'T_X2'\,'T_X3'\)"
