ffb211d7aa526a867562f738277ec2784c8bb183
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-09-20 13:57:57 +0300

    Update ghost_session_find.sql

--- ghost_session_find.sql
+++ ghost_session_find.sql
@@ -5,3 +5,12 @@ SELECT    'alter system kill session '''
        || ';'
   FROM v$locked_object lo, dba_objects ao, v$session sess
  WHERE ao.object_id = lo.object_id AND lo.session_id = sess.sid;
+
+
+
+ select s.sid, s.serial#, s.status, p.spid
+from v$session s, v$process p
+where s.username = 'LFRY'
+and p.addr (+) = s.paddr;
+
+alter system kill session '383,2036'
