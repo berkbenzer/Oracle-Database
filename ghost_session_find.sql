8520d6c1e07034f6c4f3ecbc85d43b59dd4cc718
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-09-24 13:42:59 +0300

    Update ghost_session_find.sql

--- ghost_session_find.sql
+++ ghost_session_find.sql
@@ -11,7 +11,7 @@ SELECT    'alter system kill session '''
  
  select 'alter system kill session ''' || s.sid || ',' || s.serial# || ''';'
 from v$session s, v$process p
-where s.username = 'LFRY'
+where s.username = 'XXX'
 and p.addr (+) = s.paddr;
 
 alter system kill session '383,2036'
