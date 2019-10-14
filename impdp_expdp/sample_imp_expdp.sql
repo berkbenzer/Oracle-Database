6253b2472140d41681008cbd2d2c5d1bcafe387e
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-10-14 11:21:25 +0300

    Update sample_imp_expdp.sql

--- impdp_expdp/sample_imp_expdp.sql
+++ impdp_expdp/sample_imp_expdp.sql
@@ -6,7 +6,7 @@ impdp FILE=T_PAYMENT.dmp  TABLES=T_PAYMENT_INFO_DMP
 exp \'/ as sysdba\' file=/oracle/yedek/audit_27032019.dmp log=/oracle/yedek/audit_27032019.log tables=sys.aud$
 
 
-
+--Full Schema
 exp '/ as sysdba' file=xxx_07122017.dmp log=xxx_07122017.log OWNER=TEST GRANTS=y ROWS=y
 
 imp '/ as sysdba' file=full64_07.12.2017.dmp log=full64_07.12.2017_imp.log fromuser=TEST touser=TEST ignore=y 
@@ -21,8 +21,14 @@ expdp file=XXX13092019.dmp log=XXX13092019 OWNER=XXX directory=DATA_PUMP_DIR GRA
 
 expdp dumpfile=LF6092019.dmp_%U log=LF16092019 OWNER=LF directory=DATA_PUMP_DIR GRANTS=y ROWS=y
 
-
+-- Remap schema
 impdp system DIRECTORY=DATA_PUMP_DIR DUMPFILE=XXX13092019.dmp REMAP_SCHEMA=SOURCE_SCHEMA_NAME:NEW_SCHEMA_NAME
 
--- single table
+-- Single table
 impdp file=xxx.dmp log=xxx.log tables=<schema_name>.<table_name>
+
+--Multiple Table
+expdp dumpfile=initload2.dmp logfile=initload2.log directory=DATA_PUMP_DIR tables=XXX.X1,XXX.X1
+
+-- Remap Tablespace
+impdp dumpfile=initload2.dmp logfile=imp_initialload.log directory=DATA_PUMP_DIR remap_tablespace=TS_XX:USERS
