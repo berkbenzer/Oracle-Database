256bccb6aa64fcad0d2706841f658b5f361760fe
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-01-29 08:50:53 +0300

    Update Conversion_sqls.sql

--- Conversion_sqls.sql
+++ Conversion_sqls.sql
@@ -1,6 +1,14 @@
 --NTIMESTAMP#	31			Y	TIMESTAMP(6)		None	14030848	0	0	
 
 select * from  sys.aud$ AUD
-where AUD.userid='BESPROD'
+where AUD.userid='XXXX'
 and trunc(AUD.NTIMESTAMP#) > TO_DATE('09/20/2019','MM/DD/YYYY')
 ;
+
+
+
+select * from  sys.aud$ AUD
+where AUD.userid='XXXX'
+and trunc(AUD.NTIMESTAMP#) > TO_DATE('09/20/2019 5:00:00','MM/DD/YYYY HH:MI:SS')
+;
+
