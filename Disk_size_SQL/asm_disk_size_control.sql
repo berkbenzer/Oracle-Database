c67183c7ad724d8d8272e964fdbf9286a7eeb4e5
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-02-05 08:56:30 +0300

    Update asm_disk_size_control.sql

--- Disk_size_SQL/asm_disk_size_control.sql
+++ Disk_size_SQL/asm_disk_size_control.sql
@@ -1,8 +1,8 @@
 
 SELECT G.NAME,
-       sum(b.total_mb)/1024                                  total_gb,
-       sum((b.total_mb - b.free_mb))/1024     used_gb,
-       sum(B.FREE_MB)/1024   free_gb,
+       round(sum(b.total_mb)/1024,2)                                  total_gb,
+       round(sum((b.total_mb - b.free_mb))/1024,2)     used_gb,
+       round(sum(B.FREE_MB)/1024,2)   free_gb,
        decode(sum(b.total_mb),0,0,(ROUND((1- (sum(b.free_mb) / sum(b.total_mb)))*100, 2)))      pct_used,
        decode(sum(b.total_mb),0,0,(ROUND(((sum(b.free_mb) / sum(b.total_mb)))*100, 2)))      pct_free
 FROM
