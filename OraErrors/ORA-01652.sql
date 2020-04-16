a5d75670d6ddd90e3e8da3fee5a527e4c65383f7
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-04-16 18:10:19 +0300

    ORA-01652: unable to extend temp segmen
    ORA-01652: unable to extend temp segment by 128 in tablespace SYSTEM: How to extend?

---
+++ OraErrors/ORA-01652.sql
@@ -0,0 +1,25 @@
+
+
+SELECT A.tablespace_name tablespace, D.gb_total,
+
+SUM (A.used_blocks * D.block_size) / 1024 / 1024 mb_used,
+
+D.gb_total - SUM (A.used_blocks * D.block_size) / 1024 / 1024 /1024 gb_free
+
+FROM v$sort_segment A,
+
+(
+
+SELECT B.name, C.block_size, SUM (C.bytes) / 1024 / 1024 /1024 gb_total
+
+FROM v$tablespace B, v$tempfile C
+
+WHERE B.ts#= C.ts#
+
+GROUP BY B.name, C.block_size
+
+) D
+
+WHERE A.tablespace_name = D.name
+
+GROUP by A.tablespace_name, D.gb_total;
