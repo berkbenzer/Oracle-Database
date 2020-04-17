b655d0e9e0c72540eb400457d7761f96d21a3e22
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-04-17 09:34:58 +0300

    Create log_del.sh

---
+++ DBA_BASH/log_del.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | grep  oracle |awk '{ print $5 " " $6 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $5}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  if [ $usep -ge 90 ]; then
+   find /oracle/diag/tnslsnr/DBISMI/listener/alert/   -name 'log_*' -mtime +2 -exec rm -f {} \;
+   find /oracle/diag/rdbms/orcl/orcl/alert/              -name 'log_*' -mtime +2 -exec rm -f {} \;
+   find /oracle/diag/rdbms/orcl/orcl/trace/              -name 'cdump' -mtime +2 -exec rm -f {} \;
+   echo "" > /oracle/diag/tnslsnr/TRBAHTDB/listener/trace/listener.log
+  fi
+done
