0bba1591ba89de4d3ba58db433356ffb288439c4
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-04-28 14:48:35 +0300

    Delete table_size.sql

--- Disk_size_SQL/table_size.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM   (SELECT owner,
-               segment_name,
-               segment_type,
-               tablespace_name,
-               ROUND(bytes/1024/1024/1024,2) size_GB
-        FROM   dba_segments
-        ORDER BY 5 DESC)
-WHERE  ROWNUM <= 20;
