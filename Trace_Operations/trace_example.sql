57ffb4b9c4e6d4731807159d52d80074edb25996
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-05-20 16:29:51 +0300

    Create trace_example.sql

---
+++ Trace_Operations/trace_example.sql
@@ -0,0 +1,22 @@
+alter session set tracefile_identifier='10046ERRSTK';
+
+alter session set timed_statistics = true;
+
+alter session set statistics_level= all ;
+
+alter session set max_dump_file_size = unlimited;
+
+alter session set events '10046 trace name context forever,level 12';
+
+alter session set events '12801 trace name ERRORSTACK level 3';
+
+alter session set events '2003 trace name ERRORSTACK level 3';
+
+
+------------------------------
+
+alter session set events '10046 trace name context off';
+
+alter session set events '12801 trace name errorstack off';
+
+alter session set events '2003 trace name errorstack off';
