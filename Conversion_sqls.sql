312b2258d436da78bc0aeec872a696f7effa48ad
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-06-08 16:49:22 +0300

    Update Conversion_sqls.sql

--- Conversion_sqls.sql
+++ Conversion_sqls.sql
@@ -20,3 +20,15 @@ and trunc(AUD.NTIMESTAMP#) > TO_DATE('09/20/2019 5:00:00','MM/DD/YYYY HH:MI:SS')
          AND TRUNC (SAMPLE_TIME) > TO_DATE ('28/05/2020', 'dd/mm/yyyy')       
 GROUP BY to_char (DHA.SAMPLE_TIME, 'dd/mm/yyyy'), du.username
 ORDER BY 1 ASC
+
+
+-- every 5 min display who connects DB
+  SELECT   TRUNC (timestamp, 'hh24')
+         + TRUNC (TO_CHAR (timestamp, 'mi') / 10) * 5 / 1440 as COLUMN1,
+         COUNT (*)
+    FROM DBA_AUDIT_TRAIL
+   WHERE     UPPER (USERNAME) IN ('XXXX')
+         AND TO_CHAR (TIMESTAMP, 'yyyy') NOT IN ('2017', 2018, 2019)
+GROUP BY   TRUNC (timestamp, 'hh24')
+         + TRUNC (TO_CHAR (timestamp, 'mi') / 10) * 5 / 1440
+         order by COLUMN1 ASC
