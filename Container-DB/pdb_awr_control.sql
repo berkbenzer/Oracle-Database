f2c22c66267926dbbdf4bd0334142714a4604365
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-06-23 11:11:27 +0300

    Create pdb_awr_control.sql

---
+++ Container-DB/pdb_awr_control.sql
@@ -0,0 +1,9 @@
+
+     select dbid, snap_id, instance_number from DBA_HIST_SNAPSHOT ;
+
+    select dbid, instance_number from AWR_PDB_DATABASE_INSTANCE;
+
+    
+
+select dbid, snap_id, instance_number from DBA_HIST_SNAPSHOT where dbid='1393164316' ;
+
