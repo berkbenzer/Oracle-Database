3262ecaf93f86659f43d1c16deb962fc5bdbdf27
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-07-08 11:30:52 +0300

    Update log_del.sh

--- DBA_BASH/log_del.sh
+++ DBA_BASH/log_del.sh
@@ -8,6 +8,8 @@ do
    find /oracle/diag/tnslsnr/DBISMI/listener/alert/   -name 'log_*' -mtime +2 -exec rm -f {} \;
    find /oracle/diag/rdbms/orcl/orcl/alert/              -name 'log_*' -mtime +2 -exec rm -f {} \;
    find /oracle/diag/rdbms/orcl/orcl/trace/              -name 'cdump' -mtime +2 -exec rm -f {} \;
+   find /oracle/diag/rdbms/orcl/orcl/alert/              -name '*.trc' -mtime +2 -exec rm -f {} \;
+   find /oracle/diag/rdbms/orcl/orcl/alert/              -name '*.trm' -mtime +2 -exec rm -f {} \;
    echo "" > /oracle/diag/tnslsnr/TRBAHTDB/listener/trace/listener.log
   fi
 done
