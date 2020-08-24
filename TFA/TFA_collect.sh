27e13c6e747e18bff9b96bd095c7f772800ffc29
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-08-24 22:46:52 +0300

    Create TFA_collect.sh

---
+++ TFA/TFA_collect.sh
@@ -0,0 +1,13 @@
+
+
+[root@xxxxx:/u01/app/11.2.0/grid/tfa/bin]# ./tfactl diagcollect -tag db_crash_a1 -from "Aug/24/2020 09:00:00" -to "Aug/24/2020 12:30:00"
+Collecting data for all nodes
+
+ing data for all nodes
+
+Tag already exists. Using a new tag: db_crash_a1_1
+Scanning files from Aug/24/2020 09:00:00 to Aug/24/2020 12:30:00
+
+Collection Id : xxxxxxxx
+
+Repository Location in istsl75pmsdb1p : /u01/app/grid/tfa/repository
