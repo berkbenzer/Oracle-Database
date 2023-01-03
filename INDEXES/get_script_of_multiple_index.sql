860c4ce11f1e663b838a57a8e12d402bb7c1394b
Berk Benzer <52455783+berkbenzer@users.noreply.github.com>
2023-01-03 12:18:51 +0100

    Update get_script_of_multiple_index.sql

--- INDEXES/get_script_of_multiple_index.sql
+++ INDEXES/get_script_of_multiple_index.sql
@@ -1,8 +1,7 @@
-### get single index script ###
+-- get single index script 
 select dbms_metadata.get_ddl ('INDEX', 'EMP_EMAIL_UK', 'HR') from dual;
 
-
-### get multiple index script ###
+-- return select script of each index
 
 SELECT    'select DBMS_METADATA.GET_DDL("INDEX", "'
        || index_name
@@ -11,3 +10,6 @@ SELECT    'select DBMS_METADATA.GET_DDL("INDEX", "'
        || '") from dual;'
   FROM dba_indexes
  WHERE table_name = 'TABLE_NAME';
+
+-- Get each Index Script for the specific table
+ select DBMS_METADATA.GET_DDL('INDEX', INDEX_NAME, OWNER) from DBA_INDEXES WHERE TABLE_NAME='TABLE_NAME' AND OWNER LIKE '%';
