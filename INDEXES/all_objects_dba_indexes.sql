88d82e6bffadfa900972ed8cc99194794b799d4f
Berk Benzer <52455783+berkbenzer@users.noreply.github.com>
2023-01-04 12:24:07 +0100

    Update all_objects_dba_indexes.sql

--- INDEXES/all_objects_dba_indexes.sql
+++ INDEXES/all_objects_dba_indexes.sql
@@ -6,15 +6,25 @@ column TABLE_OWNER format a30;
 column TABLESPACE_NAME format a30;
 column LAST_ANALYZED format a15;
 column CREATED format a15;
+column bytes format a10;
 
 
 
-SELECT DI.OWNER,
-       DI.INDEX_NAME,
-       DI.TABLESPACE_NAME,
-       DI.LAST_ANALYZED,
-       AO.CREATED
-  FROM ALL_OBJECTS AO, dba_indexes DI
- WHERE     1 = 1
-       AND AO.OBJECT_NAME = DI.INDEX_NAME
-       AND DI.TABLE_NAME = 'NEGOTIATION';
+  SELECT DI.OWNER,
+         DI.INDEX_NAME,
+         DI.TABLESPACE_NAME,
+         DI.LAST_ANALYZED,
+         AO.CREATED,
+         ROUND (SUM (bytes) / 1024 / 1024 / 1024, 2) GB
+    FROM ALL_OBJECTS AO, dba_indexes DI, dba_segments seg
+   WHERE     1 = 1
+         AND AO.OBJECT_NAME = DI.INDEX_NAME
+         AND di.owner = seg.owner
+         AND di.index_name = seg.segment_name
+         AND DI.TABLE_NAME = 'TABLE_NAME'
+GROUP BY DI.OWNER,
+         DI.INDEX_NAME,
+         DI.TABLESPACE_NAME,
+         DI.LAST_ANALYZED,
+         AO.CREATED
+ORDER BY GB DESC
