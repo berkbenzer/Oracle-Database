cd9c0189ef86a3bdd083dc568f350f0e595c1ab3
Berk Benzer <52455783+berkbenzer@users.noreply.github.com>
2023-06-15 15:51:13 +0200

    Update query_perf.sql

--- Statistics_Performance/query_perf.sql
+++ Statistics_Performance/query_perf.sql
@@ -22,8 +22,22 @@ set linesize 100
 select dbms_sqltune.report_tuning_task('TEST_sql_tuning_task_2') from dual;
 
 
-
+-- Gather Statistics Specific Schema
 EXEC DBMS_STATS.gather_schema_stats('SCHEMA_NAME');
 
 EXEC DBMS_STATS.gather_database_stats;
 EXEC DBMS_STATS.gather_dictionary_stats;
+
+
+
+-- Checks jobs in progress 
+ select * from dba_optstat_operations where status = 'IN PROGRESS' order by start_time desc;
+
+-- Displays stastics of the Schema
+ SELECT owner, table_name, stats_update_time
+     FROM dba_tab_stats_history
+     WHERE 1=1 
+     AND owner='SCHEMA_NAME'
+     --AND table_name='DM_SYSOBJECT_R'
+     ORDER BY owner, table_name, stats_update_time DESC
+     
